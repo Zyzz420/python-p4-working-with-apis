ebf50179c260377ff309f7078fc7bbd902792a70
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-09-19 03:28:01 -0400

    Assignment complete

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,29 @@
-
 import requests
 import json
 
 class GetPrograms:
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        try:
+            response = requests.get(URL)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            return response.content
+        except requests.exceptions.RequestException as e:
+            print(f"Error occurred during the API request: {e}")
+            return None
 
-    response = requests.get(URL)
-    return response.content
+    def program_school(self):
+        programs_list = []
+        programs = json.loads(self.get_programs() or "[]") 
+        for program in programs:
+            programs_list.append(program.get("agency"))  
 
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+if programs_schools is not None:
+    for school in set(programs_schools):
+        print(school)
